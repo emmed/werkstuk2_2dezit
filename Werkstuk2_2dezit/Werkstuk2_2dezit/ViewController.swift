e7aaee2b9ee263bf1f89050850cb05717eb6e1c7
emmed <43912976+emmed@users.noreply.github.com>
2019-08-02 17:47:19 +0200

    Add files via upload
    start of the first commit

---
+++ Werkstuk2_2dezit/Werkstuk2_2dezit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Werkstuk2_2dezit
+//
+//  Created by M_FLY on 02/08/2019.
+//  Copyright © 2019 ehb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Werkstuk2_2dezit/Werkstuk2_2dezit/testViewController.swift
@@ -0,0 +1,30 @@
+//
+//  testViewController.swift
+//  Werkstuk2_2dezit
+//
+//  Created by M_FLY on 02/08/2019.
+//  Copyright © 2019 ehb. All rights reserved.
+//
+
+import UIKit
+
+class testViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

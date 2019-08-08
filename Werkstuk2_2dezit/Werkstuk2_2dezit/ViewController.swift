868d80e070c7bfd0d7bf396b9babb6945350b9c9
Med Em <med.em@student.ehb.be>
2019-08-08 15:12:20 +0200

    Locatie user + eerste launch ingesteld

--- Werkstuk2_2dezit/Werkstuk2_2dezit/ViewController.swift
+++ Werkstuk2_2dezit/Werkstuk2_2dezit/ViewController.swift
@@ -37,6 +37,8 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         mapView.showsUserLocation = true
         mapView.delegate = self
         
+        eersteLaunch()
+        checkLocationService()
     }
 
     override func viewDidAppear(_ animated: Bool) { // alert venster
@@ -53,5 +55,76 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
      }
      */
     
+    
+    //https://stackoverflow.com/questions/27208103/detect-first-launch-of-ios-app
+    func eersteLaunch(){
+        
+        
+        
+        let Eerstelaunch = UserDefaults.standard.bool(forKey: "EersteLaunch") // declareren en gelijkstellen met als een boolean met de key "EersteLaunch"
+        
+        if !Eerstelaunch  {
+         
+            
+            UserDefaults.standard.set(true, forKey: "Eerstelaunch")
+            print("Dit is de eerste launch")
+            
+            
+            
+        } else {
+            
+            
+            print("De app is meerdere keren gelaunch geweest")
+            
+        }
+    }
+    
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+    }
+    
+    func centerViewOnUserLocation(){
+        if let location  = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 10000, longitudinalMeters: 10000)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationService() {
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuthorization()
+        }else {
+            //
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse: //als de app actief is kan het locatie gebruiken
+            mapView.showsUserLocation = true // set  Scheme/Edit Scheme/Options/Allow Location Simulation checked but don't have a default location set.
+            centerViewOnUserLocation()
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            
+            break
+        case .authorizedAlways: // als de app zelfs niet actief is.
+            break
+            
+        }
+    }
+    
+    
+    
+    
+    
 }
 
+//https://stackoverflow.com/questions/25437891/use-of-undeclared-type-in-swift-even-though-type-is-internal-and-exists-in-s

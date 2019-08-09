3bcb07fbcd8a415adf873d493b892e6c754ac1d5
Med Em <med.em@student.ehb.be>
2019-08-09 12:05:09 +0200

    core data ophalen + het opmaken van entities + ophalen en ophalen van records en coordinaten kleur aan de pololies geven en het lopen van in de inkomende coordinaten/records

--- Werkstuk2_2dezit/Werkstuk2_2dezit/ViewController.swift
+++ Werkstuk2_2dezit/Werkstuk2_2dezit/ViewController.swift
@@ -64,7 +64,8 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         let Eerstelaunch = UserDefaults.standard.bool(forKey: "EersteLaunch") // declareren en gelijkstellen met als een boolean met de key "EersteLaunch"
         
         if !Eerstelaunch  {
-         
+            dataOphalen()
+
             
             UserDefaults.standard.set(true, forKey: "Eerstelaunch")
             print("Dit is de eerste launch")
@@ -72,12 +73,68 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
             
             
         } else {
-            
+            coreDataOphalen()
             
             print("De app is meerdere keren gelaunch geweest")
             
         }
     }
+    
+    @IBAction func VernieuwData(_ sender: Any) {
+        dataOphalen() // roept de methode/function dataOphalne op voor de knop "vernieuwen"
+    }
+    
+    func coreDataOphalen(){
+        
+        mapView.removeOverlays(mapView.overlays) //
+        
+        // entity opmaken
+        let haalCoordinaten = NSFetchRequest<NSFetchRequestResult>(entityName: "Coordinaat")         //Coordinaten & Records ophalen  FetchRequest
+        let haalRecords = NSFetchRequest<NSFetchRequestResult>(entityName: "Records")
+        //  entity ophalen
+        do {
+            self.inkomendRecords = try self.managedContext!.fetch(haalRecords) as! [Records]
+            lblLaatsteGeupdate.text = inkomendRecords[0].timestamp
+            for record in self.inkomendRecords{
+                // filtering van resultaten met NSpredicate
+                haalCoordinaten.predicate = NSPredicate(format: "recordid == %@", record.recordid!)
+                self.inkomendCoordinaten = try self.managedContext!.fetch(haalCoordinaten) as! [Coordinaat]
+                if(record.level_of_service=="ORANGE"){
+                    self.color = UIColor.orange
+                }
+                if(record.level_of_service=="ROUGE"){
+                    self.color = UIColor.red
+                }
+                if(record.level_of_service=="VERT"){
+                    self.color = UIColor.green
+                }
+                var points = [CLLocationCoordinate2D]()
+                for coordinaat in self.inkomendCoordinaten {
+                    points.append(CLLocationCoordinate2D(latitude: coordinaat.latitude, longitude: coordinaat.longitude))
+                    let polyLine = MKGeodesicPolyline(coordinates: points, count: points.count)
+                    self.mapView.addOverlay(polyLine)
+                }
+                points.removeAll()
+            }
+        } catch {
+            fatalError("Kan data niet fetchen: \(error)")
+        }
+    }
+    
+    
+    func dataOphalen() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            else
+        {
+            return
+        }
+         managedContext = appDelegate.persistentContainer.viewContext
+        
+    }
+    
+    
+    
+    
     
     func setupLocationManager(){
         locationManager.delegate = self
